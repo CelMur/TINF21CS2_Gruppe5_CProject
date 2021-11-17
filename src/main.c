74f0c08d57f1ef970d8d357497db7583ff81ad02
Luis Eckert <eckert.luis@gmx.de>
2021-11-17 14:34:12 +0100

    merge files date.h, student.h
    merge date.h and student.h into main.c

--- src/main.c
+++ src/main.c
@@ -1,11 +1,33 @@
-#include "structures\date.h"
-#include "structures\student.h"
 
+#include <stdlib.h>
+
+
+typedef struct Date{
+  int day;
+  int month;
+  int year;
+}Date;
+
+typedef struct Student{
+  char *lastname;
+  char *matrikelNr;
+
+  struct Date birthday;
+  struct Date start;
+  struct Date end;
+
+  struct Student *next_node;
+  struct Student *prev_node;
+}Student;
+
+typedef struct List{
+  struct Student *first_node;
+  struct Student *last_node;
+
+  int length;
+}List;
 
 
 int main(){
-
-    Student student;
-    
-    return 1;
+  
 }
