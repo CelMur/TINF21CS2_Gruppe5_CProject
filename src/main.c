a41dbd1a56237429f83ddd251a26f3e6bf2909c1
Luis Eckert <eckert.luis@gmx.de>
2021-11-19 00:03:22 +0100

    update addStudent alphabetical sort
    main.c
    addStudent(List *list, Student *node)
    
    students get added alphabetically

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 
 #include <stdlib.h>
+#include <string.h>
 
 
 typedef struct Date{
@@ -8,9 +9,10 @@ typedef struct Date{
   int year;
 }Date;
 
+
 typedef struct Student{
-  char *lastname;
-  char *matrikelNr;
+  char lastname[50];
+  char matrikelNr[50];
 
   struct Date birthday;
   struct Date start;
@@ -20,6 +22,7 @@ typedef struct Student{
   struct Student *prev_node;
 }Student;
 
+
 typedef struct List{
   struct Student *first_node;
   struct Student *last_node;
@@ -28,20 +31,56 @@ typedef struct List{
 }List;
 
 
+int compareNodes(Student *node1, Student *node2){
+  if (node1 == NULL) return 1;
+  if (node2 == NULL) return 1;
+
+  int cmp = strcmp(node1->matrikelNr, node2->matrikelNr);
+  
+  return cmp;
+}
+
+
 void addStudent(List *list, Student *node){
 
+  Student *currentNode = list->first_node;
+  int currentCmpResult = 0;
+  int prevCmpResult = 0;
+
   if(list->first_node == NULL){
-    list->first_node = node;
-    list->last_node = node;
-    list->length++;
-    return;
+      list->first_node = node;
+      list->last_node = node;
+      list->length++;
+      return;
   }
 
+  do{
+    currentCmpResult = compareNodes(currentNode, node);
+    
+    if(currentCmpResult > 0 && prevCmpResult < 0){
 
-  Student *tempNode = &(*list->last_node);
-  tempNode->next_node = node;
-  list->last_node = node;
-  node->prev_node = tempNode;
+      Student *prevNode = currentNode->prev_node;
+      Student *nextNode = currentNode;
+
+      node->prev_node = prevNode;
+      node->next_node = nextNode;
+
+      prevNode->next_node = node;
+      nextNode->prev_node = node;
+     
+      break;
+    }
+    else if(currentNode->next_node == NULL){
+      currentNode->next_node = node;
+      node->prev_node = currentNode;
+      list->last_node = node;
+      break;
+    }
+
+    currentNode = currentNode->next_node;
+    prevCmpResult = currentCmpResult;
+
+  }while(1);
 
   list->length++;
 }
@@ -50,4 +89,36 @@ void addStudent(List *list, Student *node){
 
 int main(){
 
+  List list;
+  list.first_node = NULL;
+  list.last_node = NULL;
+  list.length = 0;
+
+  Student * s0 = (Student*) malloc(sizeof(Student));
+  strcpy(s0->lastname, "testperson1");
+  s0->prev_node = NULL;
+  s0->next_node = NULL;
+
+  Student * s1 = (Student*) malloc(sizeof(Student));
+  strcpy(s1->lastname, "testperson5");
+  s1->prev_node = NULL;
+  s1->next_node = NULL;
+
+  Student * s2 = (Student*) malloc(sizeof(Student));
+  strcpy(s2->lastname, "testperson3");
+  s2->prev_node = NULL;
+  s2->next_node = NULL;
+
+  Student * s3 = (Student*) malloc(sizeof(Student));
+  strcpy(s3->lastname, "testperson6");
+  s3->prev_node = NULL;
+  s3->next_node = NULL;
+
+
+  addStudent(&list, s0);
+  addStudent(&list, s1);
+  addStudent(&list, s2);
+  addStudent(&list, s3);
+ 
+  return 0;
 }
